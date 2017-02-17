d000c0015d5c330bd955885642c372a9abc90ac5
akbar joudy <akbar.joody@gmail.com>
2017-02-17 23:41:50 +0330

    init

--- Vue.php
+++ Vue.php
@@ -2,13 +2,80 @@
 
 namespace aki\vue;
 
+use Yii;
 /**
  * This is just an example.
  */
 class Vue extends \yii\base\Widget
 {
-    public function run()
-    {
-        return "Hello!";
+    /**
+     *
+     * @var Array
+     */
+    public $data;
+    
+    /**
+     * [
+     *  reverseMessage: function () {
+     *       this.message = this.message.split('').reverse().join('')
+     *     }
+     * ]
+     * @var Array
+     */
+    public $methods;
+
+
+    public function init() {
+        $this->view->registerAssetBundle(VueAsset::className());
+        //generate data
+//        $data = $this->generateData();
+        //generate method
+//        $methods = $this->generateMethods();
+        
+       
+        
     }
+    
+    public static function begin($config = array()) {
+        $obj =  parent::begin($config);
+        
+        $el = $obj->id;
+        $data = $obj->generateData();
+        $methods = $obj->generateMethods();
+        $js = "
+            var app = new Vue({
+                el: '#".$el."',
+                ".(!empty($data) ? "data :".$data:null).",
+                ".(!empty($methods) ? "methods :".$methods:null).",
+            }); 
+        ";
+        Yii::$app->view->registerJs($js, \yii\web\View::POS_END);
+        echo '<div id="'.$obj->id.'">';
+        return $obj;
+    }
+    
+    
+    public static function end() {
+        echo '</div>';
+        return parent::end();
+    }
+    
+    
+    
+    public function generateData() {
+        if(!empty($this->data)){
+            return json_encode($this->data);
+        }
+    }
+  
+    public function generateMethods() {
+        if(!empty($this->methods)){
+            $str = '';
+            foreach ($this->methods as $key => $value) {
+                $str.= $key.":".$value->expression;
+            }
+            return "{".$str."}";
+        }
+    }
+    
 }
